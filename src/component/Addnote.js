677ded81ad28c9b774c8af85b012fddf07256ec7
unknown <you@example.com>
2024-05-15 01:03:57 +0530

    jfsdkjgk

--- src/component/Addnote.js
+++ src/component/Addnote.js
@@ -22,15 +22,15 @@ const Addnote = (props) => {
     <form onSubmit={handleClick}> 
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
-    <input type="text" className="form-control" id="title" name='title' value={note.title} onChange={onChange} aria-describedby="emailHelp"/>
+    <input type="text" className="form-control mrr" id="title" name='title' value={note.title} onChange={onChange} aria-describedby="emailHelp"/>
      </div>
    <div className="mb-3">
     <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
-    <input type="text" className="form-control" id="description" name='description' value={note.description} onChange={onChange}  />
+    <input type="text" className="form-control mrr" id="description" name='description' value={note.description} onChange={onChange}  />
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputTag1" className="form-label">Tag</label>
-    <input type="text" className="form-control" id="tag" name='tag' value={note.tag} onChange={onChange}   />
+    <input type="text" className="form-control mrr" id="tag" name='tag' value={note.tag} onChange={onChange}   />
   </div>
   
   <button   type="submit" className="btn btn-primary"  >Add Note</button>
